dcc02e022889f9a8cefb8001d818a6b1e8b46aea
Joseph <JyeonO94@Gmail.com>
2020-12-09 14:25:41 -0500

    done

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+#Assigning default value
+def parrot(bird_sound = "Squawk!")
+    puts bird_sound
+    return bird_sound
+end
